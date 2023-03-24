0c49b6e09817f6287334381b696735068f432b81
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-03-24 10:23:49 +0100

    Add project files.

---
+++ MintPlayer.StringBuilder.Extensions.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using MintPlayer.StringBuilder.Extensions;
+using System.Text;
+
+var builder = new StringBuilder();
+builder.AppendIndented("""
+    namespace MyStringBuilder.Extensions
+    {
+        public class Demo
+        {
+    """);
+
+builder.AppendIndented("""
+        }
+    }
+    """);
+
+var result = builder.ToString();
+Console.Write(result);
