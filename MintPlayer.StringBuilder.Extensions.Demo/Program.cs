bd81785207be3369a110c8fda766bca3c66cd73d
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-03-24 10:35:20 +0100

    Finished builder

--- MintPlayer.StringBuilder.Extensions.Demo/Program.cs
+++ MintPlayer.StringBuilder.Extensions.Demo/Program.cs
@@ -5,12 +5,32 @@ var builder = new StringBuilder();
 builder.AppendIndented("""
     namespace MyStringBuilder.Extensions
     {
-        public class Demo
-        {
     """);
 
+builder.Indent(EIndentationType.Space, 4);
 builder.AppendIndented("""
-        }
+    public class Demo
+    {
+    """);
+
+builder.Indent(EIndentationType.Space, 4);
+
+builder.AppendIndented("""
+    public void Log(string message, LogLevel level)
+    {
+        Console.WriteLine(message, level);
+    }
+    """);
+
+builder.Unindent();
+
+builder.AppendIndented("""
+    }
+    """);
+
+builder.Unindent();
+
+builder.AppendIndented("""
     }
     """);
 
